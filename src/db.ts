998dfc6ebee9c75a4cfc3037b414c02a766aeb25
CodeCrafter2001 <anmolbansiwal1@gmail.com>
2026-03-17 18:19:04 +0530

    signup up route

--- src/db.ts
+++ src/db.ts
@@ -1,22 +1,23 @@
 import mongoose ,{Schema,model}from "mongoose";
+
  mongoose.connect("mongodb+srv://Brainly:Anmol%4020012004@brainly.c204sv7.mongodb.net/brainly")
 
  const UserSchema= new Schema({
     username:{type: String, unique:true},
     password: String
  });
- export const UserModel= model("User",UserSchema);
+ export const UserModel= model("User", UserSchema);
  
 const ContentSchema= new Schema({
     title: String,
     link:String,
     tags: [{type: mongoose.Types.ObjectId, ref:"Tag"}],
     type:String,
-    userId: {type:mongoose.Types.ObjectId, ref: 'U ser', required: true},
+    userId: {type:mongoose.Types.ObjectId, ref: 'User', required: true},
 })
 const LinkSchema= new Schema({
     hash: String,
     userId: {type:mongoose.Types.ObjectId, ref:'User', required:true, unique: true},
 })
-export const LinkModel= model("links", LinkSchema);
+export const LinkModel= model("Link", LinkSchema);
 export const ContentModel= model("Content", ContentSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import express from 'express';
-
+import { UserModel,LinkModel,ContentModel } from './db.js';
+import cors from "cors";
+import { JWT_PASSWORD } from './config.js';
 const app= express();
-
-app.post("api/v1/signup",(req,res)=>{
+app.use(express.json());
+app.use(cors());
+app.post("api/v1/signup",async (req,res)=>{
+    //zod validation and hash the pass
+    const username= req.body.username;
+    const password= req.body.password;
+    try{
+        await UserModel.create({
+            username: username,
+            password: password
+        })
+        res.json({
+            message:"User signed Up"
+        })
+    }catch(e){
+        res.status(401).json({
+            message:"User already exists"
+        })
+    }
     
+
 })
-app.post("api/v1/signin ",(req,res)=>{
+app.post("password/v1/signin ",(req,res)=>{
 
 })
 app.post("api/v1/content",(req,res)=>{
